fa46db859b639ee141220a78846720354a1dc0fc
Omar Salcedo <omar.salen@hotmail.com>
2023-11-17 22:50:27 -0500

    Match loop

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from typing import List
-
-user_prompt = "Enter a todo: "
-
 todos: list[str] = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    todos.append(todo)
+    user_action = input("Type add or show: ")
+
+    '''
+    Match-Case is the Switch Case of Python.
+    Here we have to first pass a parameter then try to check with which case the parameter is getting satisfied. 
+    If we find a match we will do something and if there is no match at all we will do something else.
+    '''
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+
+print("Bye!")
