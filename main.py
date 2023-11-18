a0df2f48ed669ce875d6a89f289d5edee64e0f9e
Omar Salcedo <omar.salen@hotmail.com>
2023-11-17 22:58:04 -0500

    for loop and Strip() method

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ todos: list[str] = []
 
 while True:
     user_action = input("Type add or show: ")
+    # Strip() The strip() method removes any leading, and trailing whitespaces.
+    # Leading means at the beginning of the string, trailing means at the end.
+    user_action = user_action.strip()
 
     '''
     Match-Case is the Switch Case of Python.
@@ -13,7 +16,8 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
             break
 
