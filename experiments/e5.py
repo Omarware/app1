c29a28b7ee95885ab73dbb250e7b0d423edc453e
Omar <omar.salen@hotmail.com>
2023-11-23 00:50:07 -0500

    Day 5 completed

---
+++ experiments/e5.py
@@ -0,0 +1,13 @@
+ips = ['100.122.133.105', '100.122.133.111']
+
+index = input("Enter the index of the IP you want: ")
+
+match index:
+    case '0':
+        result = f"{'You chose '}{ips[0]}"
+        print(result)
+    case '1':
+        result = f"{'You chose '}{ips[1]}"
+        print(result)
+
+        
