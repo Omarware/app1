6df2eeeecf70303596e20ed133452a158f93141b
Omar <omar.salen@hotmail.com>
2023-11-22 20:54:49 -0500

    Coming back

--- bonus/bonus4.py
+++ bonus/bonus4.py
@@ -1,1 +1,7 @@
-filenames = [ ]
+filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
+
+for filename in filenames:
+    # .replace() is a method that returns a new string
+    filename = filename.replace('.','-',1)
+    # rename()
+    # tuples can not be modified
