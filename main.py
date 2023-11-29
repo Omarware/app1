95892d8787dfee9806c65b5ea166d25b97d7210e
Omar <omar.salen@hotmail.com>
2023-11-28 22:43:04 -0500

    Day 9 Partial

--- main.py
+++ main.py
@@ -4,70 +4,62 @@ while True:
     # Leading means at the beginning of the string, trailing means at the end.
     user_action = user_action.strip()
 
-    '''
-    Match-Case is the Switch Case of Python.
-    Here we have to first pass a parameter then try to check with which case the parameter is getting satisfied. 
-    If we find a match we will do something and if there is no match at all we will do something else.
-    '''
-    match user_action:
-        case 'add':
-            # Get the todo from the user
-            todo = input("Enter a todo: ") + "\n"
+    if 'add' in user_action:
+        todo = user_action[4:]
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Add more info at the bottom
-            todos.append(todo)
+        # Add more info at the bottom
+        todos.append(todo)
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'show':
+    elif 'show' in user_action:
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # enumerate() allows you to enumerate what it is inside the function
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                # f-strings == formatting-strings to change the output of the print
-                row = f"{index + 1}--{item}"
-                print(row)
+        # enumerate() allows you to enumerate what it is inside the function
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            # f-strings == formatting-strings to change the output of the print
+            row = f"{index + 1}--{item}"
+            print(row)
 
-        case 'edit':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+    elif 'edit' in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo + '\n'
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo + '\n'
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
-            # Complete was added and method pop() to delete
-        case 'complete':
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
+        # Complete was added and method pop() to delete
+    elif 'complete' in user_action:
+        number = int(input("Number of the todo to complete: "))
 
-            number = int(input("Number of the todo to complete: "))
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
 
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
+        todos.pop(number - 1)
 
-            todos.pop(number - 1)
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+            message = f"TODO {todo_to_remove} was removed from the list."
+            print(message)
 
-                message = f"TODO {todo_to_remove} was removed from the list."
-                print(message)
-
-        case 'exit':
-            break
+    elif 'exit' in user_action:
+        break
 
 print("Bye!")
