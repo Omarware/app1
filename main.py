9c20cb7208ce5db032ebe94413892484b7a05982
Omar <omar.salen@hotmail.com>
2023-11-28 22:32:37 -0500

    Day 8 completed

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ while True:
         case 'show':
 
             with open('files/todos.txt', 'r') as file:
-                file.readlines()
+                todos = file.readlines()
 
             # enumerate() allows you to enumerate what it is inside the function
             for index, item in enumerate(todos):
@@ -34,15 +34,39 @@ while True:
                 # f-strings == formatting-strings to change the output of the print
                 row = f"{index + 1}--{item}"
                 print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
             # Complete was added and method pop() to delete
         case 'complete':
+
             number = int(input("Number of the todo to complete: "))
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
             todos.pop(number - 1)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+                message = f"TODO {todo_to_remove} was removed from the list."
+                print(message)
+
         case 'exit':
             break
 
